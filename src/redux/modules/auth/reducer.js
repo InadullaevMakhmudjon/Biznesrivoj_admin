f1bea57659e4ccdb8950b98eb8fcfe5d97fd7832
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-04-14 13:12:31 +0500

    Auth reducer for Details added

--- src/redux/modules/auth/reducer.js
+++ src/redux/modules/auth/reducer.js
@@ -5,6 +5,7 @@ const initialState = {
   loading: false,
   error: null,
   token: localStorage.getItem('token'),
+  user: JSON.parse(localStorage.getItem('user')),
 };
 
 const map = {
@@ -12,11 +13,18 @@ const map = {
   [`${types.LOGIN}${status.REJECTED}`]: (state, { payload }) => ({ ...state, loading: false, error: payload }),
   [`${types.LOGIN}${status.FULFILLED}`]: (state, { payload: { data } }) => {
     localStorage.setItem('token', data.token);
-    return { ...state, loading: true, token: data.token };
+    return { ...state, loading: false, token: data.token };
+  },
+  [`${types.DETAILS}${status.PENDING}`]: (state) => ({ ...state, loading: true }),
+  [`${types.DETAILS}${status.REJECTED}`]: () => (state, { payload }) => ({ ...state, loading: false, error: payload }),
+  [`${types.DETAILS}${status.FULFILLED}`]: (state, { payload: { data } }) => {
+    localStorage.setItem('user', JSON.stringify(data.user));
+    return { ...state, loading: false, user: data.user };
   },
   [types.LOGOUT]: (state) => {
     localStorage.removeItem('token');
-    return { ...state, token: null };
+    localStorage.removeItem('user');
+    return { ...state, token: null, user: null };
   },
 };
 
