1dd21b6be4108ff8c27fe4b14aed59159234c0ad
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-04-14 13:10:53 +0500

    Home component created

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { details } from '../../redux/modules/auth/actions';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
 
-export default () => (
-  <h1>Home</h1>
-);
+const Home = ({ children }) => {
+  const dispatch = useDispatch();
+  useEffect(() => { dispatch(details()); }, [dispatch]);
+  return (
+    <div>
+      <Sidebar />
+      <div style={{ width: 'calc(100% - 280px)', float: 'right' }}>
+        <Navbar />
+        <div style={{ marginTop: '65px', padding: '16px' }}>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Home.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
+
+export default Home;
