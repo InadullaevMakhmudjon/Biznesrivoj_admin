d440a9533a3c72dfffc6e0c58bd8880612f5f33c
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-04-14 13:12:10 +0500

    Thunk api shared to all

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,9 +2,10 @@ import { createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import api from '../../api/share';
 import reducers from './root.reducer';
 
-const middlewares = [promiseMiddleware, thunk];
+const middlewares = [promiseMiddleware, thunk.withExtraArgument(api)];
 
 const store = createStore(reducers, composeWithDevTools(applyMiddleware(...middlewares)));
 
